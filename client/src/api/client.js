f5e7b3c5d99d40a7c741bc0d924eafefb67c2c43
David Unuigbe <mowems@gmail.com>
2026-05-27 04:46:09 +0200

    Removed API debug logging

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
-
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
